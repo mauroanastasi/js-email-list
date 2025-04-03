195e386c4026f6a9383a4cda30fed0754a296abf
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-03 15:58:48 +0200

    [UPD] creo email per generare 10 volte la chiamata

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// faccio chiamata http per creare randomicamente una email
+
 axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
     console.log(resp.data.response)
 })
+
+// creo funziona che genererà 10 volte la chiamata(quindi 10 email)
+
+const generaEmail = () => {
+    for(let i=0; i<10; i++){
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+            console.log(resp.data.response)
+        })
+    }
+}
+generaEmail()
+console.log(generaEmail)
